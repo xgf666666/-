231c782782935987b26c865bfe19c126a71fa826
林锋 <lamfront@163.com>
2018-08-10 17:42:27 +0800

    个人中心1级Ui界面

--- app/src/main/java/com/microple/jademall/ui/Personal/PersonlFragment.kt
+++ app/src/main/java/com/microple/jademall/ui/Personal/PersonlFragment.kt
@@ -2,6 +2,7 @@ package com.microple.jademall.ui.Personal
 
 import android.support.v7.widget.LinearLayoutManager
 import android.view.View
+import com.chad.library.adapter.base.BaseQuickAdapter
 import com.microple.jademall.R
 import com.microple.jademall.ui.Personal.adapter.PersonlCommonAdapter
 import com.microple.jademall.ui.Personal.adapter.PersonlHelpAdapter
@@ -19,36 +20,43 @@ import kotlinx.android.synthetic.main.fragment_personl.view.*
  * date: 2018/8/8.
  * describe:
  */
-class PersonlFragment : BaseMvpViewFragment(){
+class PersonlFragment : BaseMvpViewFragment() {
 
-    private lateinit var mOrderAdapter : PersonlOrderAdapter
-    private lateinit var mSettingAdapter : PersonlSettingAdapter
-    private lateinit var mCommonAdapter : PersonlCommonAdapter
-    private lateinit var mHelpAdapter : PersonlHelpAdapter
-    private lateinit var mRootView : View
+    private lateinit var mOrderAdapter: PersonlOrderAdapter
+    private lateinit var mSettingAdapter: PersonlSettingAdapter
+    private lateinit var mCommonAdapter: PersonlCommonAdapter
+    private lateinit var mHelpAdapter: PersonlHelpAdapter
+    private lateinit var mRootView: View
     override fun getFragmentLayoutId(): Int = R.layout.fragment_personl
 
     override fun init(view: View?) {
         mRootView = view!!
         //我的订单
         mRootView.rv_myOrder.layoutManager = LinearLayoutManager(context)
-        mOrderAdapter = PersonlOrderAdapter(R.layout.item_personal_myorder, PersonOrderBean.createList())
+        mOrderAdapter = PersonlOrderAdapter(R.layout.item_personal, PersonOrderBean.createList())
         mRootView.rv_myOrder.adapter = mOrderAdapter
+        mRootView.rv_myOrder.isNestedScrollingEnabled = false
+
 
         //设置
         mRootView.rv_setting.layoutManager = LinearLayoutManager(context)
-        mSettingAdapter = PersonlSettingAdapter(R.layout.item_personal_myorder, PersonSettingBean.createList())
+        mSettingAdapter = PersonlSettingAdapter(R.layout.item_personal, PersonSettingBean.createList())
         mRootView.rv_setting.adapter = mSettingAdapter
+//        mRootView.rv_setting.isNestedScrollingEnabled = false
 
         //通用功能
         mRootView.rv_common.layoutManager = LinearLayoutManager(context)
-        mCommonAdapter = PersonlCommonAdapter(R.layout.item_personal_myorder, PersonCommonBean.createList())
+        mCommonAdapter = PersonlCommonAdapter(R.layout.item_personal, PersonCommonBean.createList())
         mRootView.rv_common.adapter = mCommonAdapter
+        mRootView.rv_common.isNestedScrollingEnabled = false
+
 
         //帮助
         mRootView.rv_help.layoutManager = LinearLayoutManager(context)
-        mHelpAdapter = PersonlHelpAdapter(R.layout.item_personal_myorder, PersonHelpBean.createList())
+        mHelpAdapter = PersonlHelpAdapter(R.layout.item_personal, PersonHelpBean.createList())
         mRootView.rv_help.adapter = mHelpAdapter
+        mRootView.rv_help.isNestedScrollingEnabled = false
+
 
     }
 }
